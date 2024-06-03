b4679867f4ce463b4be7b21b1d55a48a3836a52e
storycraft <storycraft@pancake.sh>
2024-06-03 10:05:38 +0900

    feat: update example

--- examples/hello_world/src/main.rs
+++ examples/hello_world/src/main.rs
@@ -1,21 +1,21 @@
-use std::fmt::Debug;
+use std::fmt::Display;
 
 use retained::retained;
 
 #[retained(State<T>)]
-pub fn asdf<T: Debug>(input: T, input2: &str) {
+pub fn display<T: Display>(input: T, input2: &str) {
     #[retained]
-    let ref a: T = input;
+    let ref input: T = input;
 
     #[retained]
     let ref mut nested: State2 = State2::new();
-    asdf2(input2, nested);
+    display_str(input2, nested);
 
-    dbg!(a);
+    println!("input: {input}");
 }
 
 #[retained(State2)]
-pub fn asdf2(text: &str) {
+pub fn display_str(text: &str) {
     #[retained]
     let ref text: String = text.to_string();
 
@@ -25,9 +25,16 @@ pub fn asdf2(text: &str) {
 fn main() {
     let state = &mut State::new();
 
-    asdf(&123, "Hello world", state);
-    asdf(&456, "", state);
-    asdf(&789, "", state);
-    asdf(&012, "world", state);
-    asdf(&345, "Hello", state);
+    // initialize states and print
+    //
+    // text: Hello world
+    // input: 123
+    display(&123, "Hello world", state);
+
+    // different arguments are given,
+    // but print same as first since states are already initialized.
+    display(&456, "", state);
+    display(&789, "", state);
+    display(&012, "world", state);
+    display(&345, "Hello", state);
 }
